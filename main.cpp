67cbea5a450d5073334edf7ab5217626f0a0d50c
Samuel Baysting <samuelbaysting@gmail.com>
2015-09-21 19:23:58 -0400

    added comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -10,20 +10,23 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
 
+    // Create new instance of the lidar data structure to store data
     lidar_data* data = new lidar_data(360*2);
 
+    // Start the LIDAR scanner
     if(data->startScan() == false){
         return 2;
     }
 
+    // Run update and display data in a loop
     while(1){
-        if(data->updateData() == true){
-            for (uint pos = 0; pos < data->count ; ++pos) {
+        if(data->updateData() == true){ // If the data update succeeded
+            for (uint pos = 0; pos < data->count ; ++pos) { // Loop through each data node
                 printf("%s theta: %03.2f Dist: %08.2f Q: %d \n",
-                    data->getSyncBit(pos) ?"S ":"  ",
-                    data->getTheta(pos),
-                    data->getDistance(pos),
-                    data->getQuality(pos));
+                    data->getSyncBit(pos) ?"S ":"  ", //Display sync bit
+                    data->getTheta(pos), //Display theta value for that node
+                    data->getDistance(pos), //Display distance value for that node
+                    data->getQuality(pos)); //Display quality value for that node
             }
         }
     }
